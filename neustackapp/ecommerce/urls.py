d6e1e9e4a4db3ae601facf75028888b4dca724a1
Rishabh Verma <rishabh.verma11998@gmail.com>
2026-06-25 15:20:16 +0530

    Add: ecommerce app boilerplate

---
+++ neustackapp/ecommerce/urls.py
@@ -0,0 +1,37 @@
+from django.urls import path
+
+from store.views import (
+    AddToCartAPIView,
+    CartAPIView,
+    CheckoutAPIView,
+    GenerateDiscountAPIView,
+    StatsAPIView
+)
+
+urlpatterns = [
+
+    path(
+        "cart/items/",
+        AddToCartAPIView.as_view()
+    ),
+
+    path(
+        "cart/<str:customer_id>/",
+        CartAPIView.as_view()
+    ),
+
+    path(
+        "checkout/",
+        CheckoutAPIView.as_view()
+    ),
+
+    path(
+        "admin/discount/",
+        GenerateDiscountAPIView.as_view()
+    ),
+
+    path(
+        "admin/stats/",
+        StatsAPIView.as_view()
+    ),
+]
